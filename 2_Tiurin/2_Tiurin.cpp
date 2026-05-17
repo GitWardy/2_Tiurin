5b6511c9698faaa39651e0abada8b20b2aabf43b
Stanislaw <fase-w@bk.ru>
2026-05-17 10:28:27 +0300

    Реализованы конструкторы, деструктор и оператор присваивания ExprNode

--- 2_Tiurin/2_Tiurin.cpp
+++ 2_Tiurin/2_Tiurin.cpp
@@ -3,29 +3,53 @@
 // Инициализация статического счётчика
 int ExprNode::nextID = 0;
 
-// Конструкторы и деструктор (простые заглушки)
+
+/**
+ * Конструктор для узла-переменной
+ */
 ExprNode::ExprNode(NodeType t, char n)
     : type(t), name(n), left(nullptr), right(nullptr)
 {
-    ID = nextID++;
+    ID = nextID++;   // присвоить уникальный ID
 }
 
+/**
+ * Конструктор для узла-операции
+ */
 ExprNode::ExprNode(NodeType t, ExprNode* l, ExprNode* r)
     : type(t), name(0), left(l), right(r)
 {
-    ID = nextID++;
+    ID = nextID++;   // присвоить уникальный ID
 }
 
+
+/**
+ * Деструктор: рекурсивно удаляет левoгo и правoгo потомков
+ */
 ExprNode::~ExprNode()
 {
-    // заглушка
+    if (left) delete left;
+    if (right) delete right;                                                  
 }
 
+/**
+ * Оператор присваивания: кoпирует type, name, left, right.
+ * Поле ID не кoпируется (остаётся уникальным).
+ */
 ExprNode& ExprNode::operator=(const ExprNode& other)
 {
-    // заглушка
+    if (this != &other)
+    {
+        type = other.type;
+        name = other.name;
+        left = other.left;
+        right = other.right;
+    }
+
     return *this;
 }
+
+
 
 vector<Token> tokenize(const string& expr)
 {
